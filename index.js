507089b70fca9004521426e37a7eb0ed0b645ae0
MOHAMMED AVESH ANSARI <36327898+ansariaves@users.noreply.github.com>
2024-06-23 10:26:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+//Restart game button
+var restart = document.querySelector("#restartButton");
+
+//grabs all the squares
+var squares = document.querySelectorAll('td');
+// clear all the squares
+function clearBoard() {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].textContent = '';
+  }
+}
+
+restart.addEventListener('click', clearBoard);
+// check the sqaure marker
+
+function changeMarker() {
+  if (this.textContent === '') {
+    this.textContent = 'X';
+  }else if (this.textContent === 'X') {
+    this.textContent = 'O';
+  }else {
+    this.textContent = '';
+  }
+}
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click',changeMarker)
+}
+
+//for loop to add event listeners to all the squares
